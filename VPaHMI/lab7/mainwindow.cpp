63adfe0da370592be855c249a41c14dbda493081
MangriMen <mangrimen@gmail.com>
2021-09-08 01:15:46 +0700

    VPaHMI added lab7 part

--- VPaHMI/lab7/mainwindow.cpp
+++ VPaHMI/lab7/mainwindow.cpp
@@ -1,11 +1,37 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QLabel>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QGraphicsScene* scene = new QGraphicsScene;
+
+    QLabel* test = new QLabel;
+    test->setPixmap(QPixmap::fromImage(QImage(":/AppleTree.png")));
+
+    scene->addWidget(test);
+    QPolygon* poly = new QPolygon(QRect(0, 0, 100, 100));
+    QBrush brush;
+    brush.setColor(Qt::red);
+    QPen pen(Qt::green);
+    scene->addPolygon(*poly);
+
+    scene->setSceneRect(0, 0, 100, 100);
+
+    ui->graphicsView->setScene(scene);
+    ui->graphicsView->adjustSize();
+}
+
+void MainWindow::resizeEvent(QResizeEvent *) {
+    QRectF bounds = ui->graphicsView->scene()->itemsBoundingRect();
+    bounds.setWidth(bounds.width()*0.9);
+    bounds.setHeight(bounds.height()*0.9);
+    ui->graphicsView->fitInView(bounds, Qt::KeepAspectRatio);
+    ui->graphicsView->centerOn(0, 0);
 }
 
 MainWindow::~MainWindow()
