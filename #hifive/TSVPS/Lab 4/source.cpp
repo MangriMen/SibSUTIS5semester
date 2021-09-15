247d997e36206127b65e7b6d7e5d29b38c1a1236
NPanin <subject6gfrt@gmail.com>
2021-09-16 00:40:16 +0700

    TSVPS Lab 4 fixed

--- #hifive/TSVPS/Lab 4/source.cpp
+++ #hifive/TSVPS/Lab 4/source.cpp
@@ -3,13 +3,24 @@
 #include <vector>
 #include <string>
 #include <complex>
+#include <tuple>
 using namespace std;
 
 #define myPI 3.1415926535
 
-template <typename T>
-string getSpacedElement(T element) {
-	return ((element >= 0 ? " " : "") + to_string(element));
+namespace utils {
+	template <typename T>
+	string getSpacedElement(T element) {
+		return ((element >= 0 ? " " : "") + to_string(element));
+	}
+
+	tuple<size_t, size_t> getNumberDecompositionBy2(size_t number) {
+		for (int i = sqrt(number); i <= number; ++i) {
+			if (number % i == 0) {
+				return { i, number / i };
+			}
+		}
+	}
 }
 
 namespace arrayTweaks {
@@ -51,8 +62,8 @@ namespace arrayTweaks {
 		cout.precision(6);
 		cout << fixed;
 		for (size_t i = 0; i < arr.size(); i += 1) {
-			cout << getSpacedElement(arr[i].real()) << " "
-				<< getSpacedElement(arr[i].imag()) << endl;
+			cout << utils::getSpacedElement(arr[i].real()) << " "
+				<< utils::getSpacedElement(arr[i].imag()) << endl;
 		}
 
 		cout << endl;
@@ -99,46 +110,98 @@ vector<double> getReverseDiscreteFourierTransform(const vector<complex<double>>&
 vector<complex<double>> getSemiFastFourierTransform(const vector<double>& data) {
 	if (data.size() == 0) return vector<complex<double>>();
 
-	size_t p1 = sqrt(data.size());
-	size_t p2 = p1;
-	const double REVERSE_P1 = 1.0 / static_cast<double>(p1);
-	const double REVERSE_P2 = 1.0 / static_cast<double>(p2);
-	const double EXPR_PART = -2.0 * myPI * REVERSE_P1;
+	size_t p1, p2;
+	tie(p1, p2) = utils::getNumberDecompositionBy2(data.size());
+
 	vector<vector<complex<double>>> temp1(p1, vector<complex<double>>(p1));
 	vector<vector<complex<double>>> temp2(p1, vector<complex<double>>(p1));
-	vector<complex<double>> resultArray(p1 * p2);
 
-	for (size_t k1 = 0; k1 < p1; k1 += 1) {
-		for (size_t j2 = 0; j2 < p2; j2 += 1) {
-			for (size_t j1 = 0; j1 < p1; j1 += 1) {
+	double REVERSE_P1 = 1.0 / static_cast<double>(p1);
+	double REVERSE_P2 = 1.0 / static_cast<double>(p2);
+	const double EXPR_PART = -2.0 * myPI * REVERSE_P1;
+
+	for (size_t k1 = 0; k1 < p1; k1++) {
+		for (size_t j2 = 0; j2 < p2; j2++) {
+			for (size_t j1 = 0; j1 < p1; j1++) {
 				temp1[k1][j2] += data[j2 + p2 * j1] * exp(1i * EXPR_PART * (static_cast<double>(j1) * static_cast<double>(k1)));
 			}
 			temp1[k1][j2] *= REVERSE_P1;
 		}
 	}
-	for (size_t k1 = 0; k1 < p1; k1 += 1) {
-		for (size_t k2 = 0; k2 < p2; k2 += 1) {
-			for (size_t j2 = 0; j2 < p2; j2 += 1) {
+
+	for (size_t k1 = 0; k1 < p1; k1++) {
+		for (size_t k2 = 0; k2 < p2; k2++) {
+			for (size_t j2 = 0; j2 < p2; j2++) {
 				temp2[k1][k2] += temp1[k1][j2] * exp(1i * EXPR_PART * (static_cast<double>(j2) * (static_cast<double>(k1) + static_cast<double>(p1) * static_cast<double>(k2)) / static_cast<double>(p2)));
 			}
 			temp2[k1][k2] *= REVERSE_P2;
 		}
 	}
 
-	for (size_t k1 = 0, j = 0; k1 < p1; k1 += 1) {
-		for (size_t k2 = 0; k2 < p2; k2 += 1, j += 1) {
-			resultArray[j] = temp2[k1][k2];
+	temp1.clear();
+	temp1.~vector();
+
+	vector<complex<double>> out(p1 * p2);
+	for (size_t k1 = 0, j = 0; k1 < p2; k1++) {
+		for (size_t k2 = 0; k2 < p1; k2++, j++) {
+			out[j] = temp2[k2][k1];
 		}
 	}
 
-	return resultArray;
+	temp2.clear();
+	temp2.~vector();
+
+	return out;
+}
+
+vector<double> getReverseSemiFastFourierTransform(const vector<complex<double>>& data) {
+	if (data.size() == 0) return vector<double>();
+
+	size_t p1, p2;
+	tie(p1, p2) = utils::getNumberDecompositionBy2(data.size());
+
+	vector<vector<complex<double>>> temp1(p1, vector<complex<double>>(p1));
+	vector<vector<complex<double>>> temp2(temp1);
+
+	const double REVERSE_P1 = 1.0 / static_cast<double>(p1);
+	const double EXPR_PART = 2.0 * myPI * REVERSE_P1;
+
+	for (size_t k1 = 0; k1 < p1; k1++) {
+		for (size_t j2 = 0; j2 < p2; j2++) {
+			for (size_t j1 = 0; j1 < p1; j1++) {
+				temp1[k1][j2] += data[j2 + p2 * j1] * exp(1i * EXPR_PART * (static_cast<double>(j1) * static_cast<double>(k1)));
+			}
+		}
+	}
+
+	for (size_t k1 = 0; k1 < p1; k1++) {
+		for (size_t k2 = 0; k2 < p2; k2++) {
+			for (size_t j2 = 0; j2 < p2; j2++) {
+				temp2[k1][k2] += temp1[k1][j2] * exp(1i * EXPR_PART * (static_cast<double>(j2) * (static_cast<double>(k1) + static_cast<double>(p1) * static_cast<double>(k2)) / static_cast<double>(p2)));
+			}
+		}
+	}
+
+	temp1.clear();
+	temp1.~vector();
+
+	vector<double> out(p1 * p2);
+	for (size_t k1 = 0, j = 0; k1 < p2; k1++) {
+		for (size_t k2 = 0; k2 < p1; k2++, j++) {
+			out[j] = temp2[k2][k1].real();
+		}
+	}
+
+	temp2.clear();
+	temp2.~vector();
+
+	return out;
 }
 
 int main() {
 	srand(time(NULL));
 
 	const int VECTOR_SIZE = 16;
-
 	vector<double> startArray(VECTOR_SIZE, 0);
 	
 	for (int i = 0; i < VECTOR_SIZE; i += 1) {
@@ -148,11 +211,13 @@ int main() {
 	vector<complex<double>> discreteFourier = getDiscreteFourierTransform(startArray);
 	vector<double> reverseDiscreteFourier = getReverseDiscreteFourierTransform(discreteFourier);
 	vector<complex<double>> semiFastFourier = getSemiFastFourierTransform(startArray);
+	vector<double> reverseSemiFastFourier = getReverseSemiFastFourierTransform(semiFastFourier);
 
 	arrayTweaks::printArrayLog(startArray, "Original array:");
 	arrayTweaks::printArrayLog(discreteFourier, "Array aquired by DF:");
 	arrayTweaks::printArrayLog(reverseDiscreteFourier, "Array aquired by RDF:");
 	arrayTweaks::printArrayLog(semiFastFourier, "Array aquired by SFF:");
+	arrayTweaks::printArrayLog(reverseSemiFastFourier, "Array aquired by RSFF:");
 
 	return 0;
 }
