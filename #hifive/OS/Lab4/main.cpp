543325d24742a4fd5a6ac922fac832d6518b1bfb
NPanin <subject6gfrt@gmail.com>
2021-09-26 14:07:50 +0700

    OS lab 4 done

--- #hifive/OS/Lab4/main.cpp
+++ #hifive/OS/Lab4/main.cpp
@@ -1,25 +1,93 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <fstream>
 using namespace std;
+
+vector<string> split(string phrase, string delimiter)
+{
+    vector<string> list;
+    string s = string(phrase);
+    size_t pos = 0;
+    string token;
+
+    while ((pos = s.find(delimiter)) != string::npos)
+    {
+        token = s.substr(0, pos);
+        list.push_back(token);
+        s.erase(0, pos + delimiter.length());
+    }
+
+    list.push_back(s);
+
+    return list;
+}
 
 int main()
 {
-    string inna = "";
+    string lineIn;
 
     while (true)
     {
-        cin >> inna;
+        getline(cin, lineIn);
 
-        vector<string> saske;
+        vector<string> args = split(lineIn, " ");
 
-        if (inna == "touch")
+        if (args.size() > 0 && args[0] != "")
         {
-            // NUL >./
-        }
-        else if (inna == "mv")
-        {
-            system("copy args[1] args[2]");
-            system("del /f args[1]");
+            string linuxCommand = args[0];
+            string commandToExecute("");
+
+            if (linuxCommand == "touch")
+            {
+                commandToExecute = "type NUL > " + args[1];
+            }
+            else if (linuxCommand == "cat")
+            {
+                commandToExecute = "type " + args[1];
+            }
+            else if (linuxCommand == "mkdir")
+            {
+                commandToExecute = lineIn;
+            }
+            else if (linuxCommand == "pkill")
+            {
+                commandToExecute = "taskkill /IM " + args[1];
+            }
+            else if (linuxCommand == "clear")
+            {
+                commandToExecute = "cls";
+            }
+            else if (linuxCommand == "whoami")
+            {
+                system("whoami > temp");
+                ifstream fileIn("temp", ios::in);
+                string tmpStr("");
+
+                if (fileIn.is_open())
+                {
+                    fileIn >> tmpStr;
+                }
+
+                vector<string> output = split(tmpStr, "\\");
+                cout << output[1] << endl;
+
+                continue;
+            }
+            else if (linuxCommand == "hostname")
+            {
+                commandToExecute = lineIn;
+            }
+            else if (linuxCommand == "exit")
+            {
+                exit(EXIT_SUCCESS);
+            }
+            else
+            {
+                commandToExecute = lineIn;
+            }
+
+            system(commandToExecute.c_str());
         }
     }
 
