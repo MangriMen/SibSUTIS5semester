1771c962f876317eed1bffa6bb4beea56f127ffa
NPanin <subject6gfrt@gmail.com>
2021-09-26 18:58:25 +0700

    FLP Lab3 fixed

--- #hifive/FLP/Lab3.lisp
+++ #hifive/FLP/Lab3.lisp
@@ -1,43 +1,41 @@
 ;lab3
 ;First
-(defun equal_p(l1 l2)
+(defun isEqual(l1 l2)
     (cond
         ((null l1) t)
-        ((equal (car l1) (car l2)) (equal_p (cdr l1) (cdr l2)))
-        (t nil))
+        ((equal (car l1) (car l2)) (isEqual (cdr l1) (cdr l2)))
+        (t nil)
+		)
 )
 
-(equal_p '(1 2) '(1 2))
+(isEqual '(1 2) '(1 2))
 
 ;Second
-((1 2 3)(4 5 6)(7 8 9))
 (defun ccar (lst)
      (cond ((null lst) nil)
            (t (cons (caar lst) (ccar (cdr lst)))))
 )
-(1 4 7)
 
 (defun ccdr (lst)
      (cond ((null lst) nil)
            (t (cons (cdar lst) (ccdr (cdr lst)))))
 )
-((2 3)(5 6)(8 9))
 
-(defun transp-r (matr)
+(defun transpose (matr)
   (cond ((null (car matr)) Nil)
-        (t (cons (ccar matr) (transp-r (ccdr matr)))))
+        (t (cons (ccar matr) (transpose (ccdr matr)))))
 )
 
-(transp-r '((1 2 3)(4 5 6)(7 8 9)))
+(transpose '((1 2 3)(4 5 6)(7 8 9)))
 
 ;Third
-(defun f (p lst)
+(defun func (p lst)
   (cond ((null lst) nil)
         ((funcall p (car lst)) t)
-        (t (f p (cdr lst))))
+        (t (func p (cdr lst))))
 )
 
-(f (lambda (x) (cond((< x 0) t))) '(1 2 -3 4))
-(f (lambda (x) (cond((< x 0) t))) '(1 2 3 4))
-(f 'symbolp '(a 1 2 3))
-(f 'symbolp '(1 1 2 3))
+(func (lambda (x) (cond((< x 0) t))) '(1 2 -3 4))
+(func (lambda (x) (cond((< x 0) t))) '(1 2 3 4))
+(func 'symbolp '(a 1 2 3))
+(func 'symbolp '(1 1 2 3))
