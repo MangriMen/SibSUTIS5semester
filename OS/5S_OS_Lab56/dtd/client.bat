3955b98904800e8a30e57a355c10e6ae472d6806
MangriMen <mangrimen@gmail.com>
2021-10-04 18:15:54 +0700

    OS lab56 done

--- OS/5S_OS_Lab56/dtd/client.bat
+++ OS/5S_OS_Lab56/dtd/client.bat
@@ -1,4 +1,6 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+DEL client.exe
+DEL client.exe.manifest
 rc client.rc %*
 cl client.cpp client.res user32.lib %*
-client.exe
+DEL client.obj
--- OS/5S_OS_Lab56/dtd/client.cpp
+++ OS/5S_OS_Lab56/dtd/client.cpp
@@ -11,6 +11,8 @@ LRESULT CALLBACK
 DlgProc(HWND hDlg, UINT message, WPARAM wParam,
         LPARAM lParam);
 
+BOOL CenterWindowOnDesktop(HWND hwndWindow);
+
 int WINAPI WinMain(HINSTANCE hInstance,
                    HINSTANCE hPrevInstance,
                    LPSTR lpCmdLine,
@@ -32,26 +34,40 @@ LRESULT CALLBACK
 DlgProc(HWND hDlg, UINT message, WPARAM wParam,
         LPARAM lParam)
 {
-    TCHAR idText[100];
-    TCHAR strText[100];
+    TCHAR idText[100] = {'\0'};
+    TCHAR strText[1024] = {'\0'};
     switch (message)
     {
     case WM_INITDIALOG:
-        _stprintf(strText, TEXT("%d"), rand() % 1000 + 1);
+        _stprintf(strText, TEXT("%d"), rand() % 10000 + 1);
         SetWindowText(hDlg, strText);
+        CenterWindowOnDesktop(hDlg);
         return TRUE;
     case WM_COPYDATA:
-        SetDlgItemText(hDlg, IDC_HISTORY, (LPTSTR)(((COPYDATASTRUCT *)lParam)->lpData));
-        break;
+    {
+        HWND hwnd_idc_history = GetDlgItem(hDlg, IDC_HISTORY);
+        PTCHAR inputMessage = (LPTSTR)(((PCOPYDATASTRUCT)lParam)->lpData);
+        PTCHAR buf = new TCHAR[_tcslen(idText) + _tcslen(inputMessage)];
+
+        GetWindowText(GetParent((HWND)wParam), idText, sizeof(idText) / sizeof(idText[0]));
+
+        _stprintf(buf, TEXT("%s: %s\r\n\r"), (LPTSTR)idText, (LPTSTR)(((PCOPYDATASTRUCT)lParam)->lpData));
+        SendMessage(hwnd_idc_history, EM_SETSEL, 0, -1);
+        SendMessage(hwnd_idc_history, EM_SETSEL, -1, -1);
+        SendMessageA(hwnd_idc_history, EM_REPLACESEL, 0, (LPARAM)(buf));
+
+        delete[] buf;
+    }
+        return TRUE;
     case WM_COMMAND:
         switch (LOWORD(wParam))
         {
         case ID_SEND:
-            GetDlgItemText(hDlg, IDC_MESSAGE, idText, sizeof(idText) * sizeof(idText[0]));
-            GetDlgItemText(hDlg, IDC_MESSAGE, strText, sizeof(strText) * sizeof(strText[0]));
+            GetDlgItemText(hDlg, IDC_NAME, idText, sizeof(idText) / sizeof(idText[0]));
+            GetDlgItemText(hDlg, IDC_MESSAGE, strText, sizeof(strText) / sizeof(strText[0]));
             HWND hWnd;
             COPYDATASTRUCT data;
-            hWnd = FindWindow(TEXT("#32770"), idText);
+            hWnd = FindWindow(TEXT("#32770"), (LPTSTR)idText);
             data.cbData = _tcslen(strText);
             data.lpData = strText;
             SendMessage(hWnd, WM_COPYDATA,
@@ -62,8 +78,35 @@ DlgProc(HWND hDlg, UINT message, WPARAM wParam,
             PostQuitMessage(0);
             return TRUE;
         }
-        break;
+        return TRUE;
     default:
         return FALSE;
     }
+}
+
+BOOL CenterWindowOnDesktop(HWND hwndWindow)
+{
+    int screenwidth, screenheight;
+    int dlgwidth, dlgheight, x, y;
+    RECT rect;
+
+    /* Get Screen width and height */
+    screenwidth = GetSystemMetrics(SM_CXSCREEN);
+    screenheight = GetSystemMetrics(SM_CYSCREEN);
+
+    /* Get Window rect top, left, right, bottom */
+    GetWindowRect(hwndWindow, &rect);
+
+    /* Calculate Window width and height */
+    dlgwidth = rect.right - rect.left;
+    dlgheight = rect.bottom - rect.top;
+
+    /* Calculate Window left, top (x,y) */
+    x = (screenwidth - dlgwidth) / 2;
+    y = (screenheight - dlgheight) / 2;
+
+    /* Reposition Window left, top (x,y) */
+    SetWindowPos(hwndWindow, NULL, x, y, 0, 0, SWP_NOSIZE);
+
+    return TRUE;
 }
