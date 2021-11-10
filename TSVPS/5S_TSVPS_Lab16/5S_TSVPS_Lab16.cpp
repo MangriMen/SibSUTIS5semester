3754285c8ad89b932746269a09f8f780800878a4
MangriMen <mangrimen@gmail.com>
2021-11-11 00:06:35 +0700

    TSVPS lab16 done

--- TSVPS/5S_TSVPS_Lab16/5S_TSVPS_Lab16.cpp
+++ TSVPS/5S_TSVPS_Lab16/5S_TSVPS_Lab16.cpp
@@ -1,236 +1,113 @@
-#include <iostream>
+п»ї#include <iostream>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
-enum MethodType
+double unboundedKnapsack(int maxWeight, const vector<int>& items, const vector<double>& costs, vector<int>& itemsInBackpack, int min = -1)
 {
-	ExhausiveSearchRec,
-	BranchAndBound
-};
-
-class Item
-{
-public:
-	double weight, cost;
-
-	Item(double weight = 1, double cost = 1)
-	{
-		weight = weight;
-		cost = cost;
-	}
-};
-
-class AbstractBackpack
-{
-public:
-	double maxWeight;
-
-	MethodType Method;
-
-	AbstractBackpack(double maxWeight)
-	{
-		this->maxWeight = maxWeight;
+	if (maxWeight < 0) {
+		return 0;
 	}
 
-	double TotalCost(vector<Item>& items, vector<bool>& inBackpack)
+	vector<double> summ(items.size());
+	vector<vector<int>> Faa(items.size(), vector<int>(summ.size()));
+
+	itemsInBackpack.clear();
+	itemsInBackpack.resize(items.size());
+	itemsInBackpack.shrink_to_fit();
+
+	size_t r = 0;
+
+	for (size_t i = 0; i < summ.size(); i++)
 	{
-		double result = 0;
-		for (int i = 0; i < items.size(); i++)
+		summ[i] = (unboundedKnapsack(maxWeight - items[i], items, costs, Faa[i], static_cast<int>(i)));
+		r += !summ[i];
+	}
+
+	if (r == summ.size())
+	{
+		itemsInBackpack[min]++;
+		return costs[min];
+	}
+	else
+	{
+		r = max_element(summ.begin(), summ.end()) - summ.begin();
+		if (min == -1)
 		{
-			if (inBackpack[i])
-				result += items[i].cost;
-		}
-		return result;
-	}
-
-	double TotalCost(vector<Item>& items, vector<unsigned>& inBackpack)
-	{
-		double result = 0;
-		for (int i = 0; i < items.size(); i++)
-			result += inBackpack[i] * items[i].cost;
-		return result;
-	}
-
-	virtual vector<unsigned> SolveUnlimited(vector<Item>& items) = 0;
-
-	double TotalWeight(vector<Item>& items, vector<bool>& inBackpack)
-	{
-		double result = 0;
-		for (int i = 0; i < items.size(); i++)
-		{
-			if (inBackpack[i])
-				result += items[i].weight;
-		}
-		return result;
-	}
-
-	double TotalWeight(vector<Item>& items, vector<unsigned>& inBackpack)
-	{
-		double result = 0;
-		for (int i = 0; i < items.size(); i++)
-			result += inBackpack[i] * items[i].weight;
-		return result;
-	}
-};
-
-class BranchAndBoundBackpack : public AbstractBackpack
-{
-public:
-	class OrderedItem : public Item
-	{
-	public:
-		int number;
-		
-		OrderedItem() {}
-
-		OrderedItem(double w, double c, int i) : Item(w, c)
-		{
-			this->number = i;
-		}
-	};
-
-	vector<bool> result;
-	vector<Item> items;
-	vector<unsigned> resultUnl;
-	double maxCost;
-
-	BranchAndBoundBackpack(double maxWeight = 100) : AbstractBackpack(maxWeight)
-	{
-
-	}
-
-	//override MethodType Method
-	//{
-	//	get { return MethodType.BranchAndBound; }
-	//}
-
-	vector<unsigned> SolveUnlimited(vector<Item>& items)
-	{
-		auto defaultOrder = GetDefaultOrder(items);
-		auto ordered(defaultOrder);
-		auto kekF = [](OrderedItem const& s1, OrderedItem const& s2) -> bool
-		{
-			return static_cast<bool>(s1.cost / s1.weight);
-		};
-		sort(ordered.begin(), ordered.end(), kekF);
-		//auto ordered = defaultOrder.OrderByDescending<OrderedItem, double>
-		//	((i) = > i.cost / i.weight)
-		//	.ToArray();
-		for (size_t i = 0; i < this->items.size(); i++)
-		{
-			this->items[i] = ordered[i];
-		}
-		//this->items = ordered;
-		maxCost = 0;
-		resultUnl = vector<unsigned>(items.size());
-		auto testVector = vector<unsigned>(items.size());
-		SolveUnlimitedRec(testVector);
-		return RestoreUnlimitedSolution(ordered);
-	}
-
-	/// <summary>
-	/// Получает массив упорядоченных элементов.
-	/// </summary>
-	/// <param name="items">Массив неупорядоченных элементов.</param>
-	/// <returns>Возвращает массив упорядоченных элементов.</returns>
-	vector<OrderedItem> GetDefaultOrder(vector<Item>& items)
-	{
-		vector<OrderedItem> temp(items.size());
-		for (int i = 0; i < items.size(); i++)
-		{
-			temp[i] = OrderedItem(items[i].weight, items[i].cost, i);
-		}
-		return temp;
-	}
-	/// <summary>
-	/// Восстанавливает порядок решения, используя упорядоченную последовательность.
-	/// </summary>
-	/// <param name="items">Массив упорядоченных элементов.</param>
-	/// <returns>Возвращает решене задачи.</returns>
-	vector<bool> RestoreSolution(vector<OrderedItem>& items)
-	{
-		vector<bool> res(items.size());
-		for (int i = 0; i < items.size(); i++)
-		{
-			res[items[i].number] = result[i];
-		}
-		return res;
-	}
-	/// <summary>
-	/// Восстанавливает порядок решения, используя упорядоченную последовательность.
-	/// </summary>
-	/// <param name="items">Массив упорядоченных элементов.</param>
-	/// <returns>Возвращает решене задачи.</returns>
-	vector<unsigned> RestoreUnlimitedSolution(vector<OrderedItem>& items)
-	{
-		vector<unsigned> res(items.size());
-		for (int i = 0; i < items.size(); i++)
-		{
-			res[items[i].number] = resultUnl[i];
-		}
-		return res;
-	}
-
-	/// <summary>
-	/// Рекурсивная процедура решения задачи о неограниченном рюкзаке .
-	/// </summary>
-	/// <param name="vector">Проверяемое решение.</param>
-	/// <param name="i">Номер проверяемого элемента решения.</param>
-	/// <param name="c">Общая стоимость решения к текущему моменту.</param>
-	/// <param name="w">Общий вес решения к текущему моменту.</param>
-	void SolveUnlimitedRec(vector<unsigned>& vec, int i = 0, double c = 0, double w = 0)
-	{
-		if (i == vec.size()) //надо принять решение
-		{
-			//к этому шагу мы дойдём, только если вес рюкзака будет меньше допустимого, 
-			//т.к. проверки выполняются до запуска ветви
-			if (c > maxCost)
-			{
-				maxCost = c;
-				//resultUnl = vec.
-				//vec.CopyTo(resultUnl, 0);
-			}
+			Faa[r].swap(itemsInBackpack);
+			return summ[r];
 		}
 		else
 		{
-			double oneMoreC = c + items[i].cost;
-			double oneMoreW = w + items[i].weight;
-			if (oneMoreW <= maxWeight) //не перебираем по весу
-			{
-				vec[i]++;
-				//если оценка сверху больше масимальной стоимости
-				if (CalculateUpperBound(oneMoreC, oneMoreW, i - 1) > maxCost)
-					SolveUnlimitedRec(vec, i, oneMoreC, oneMoreW);
-			}
-			//если оценка сверху больше масимальной стоимости
-			if (CalculateUpperBound(c, w, i) > maxCost) {
-				SolveUnlimitedRec(vec, i + 1, c, w);
-			}
-			vec[i] = vec[i] > 0 ? vec[i] - 1 : 0;
+			itemsInBackpack.swap(Faa[r]);
+			itemsInBackpack[min]++;
+			return summ[r] + costs[min];
 		}
 	}
+}
 
-	double CalculateUpperBound(double c, double w, int i)
+void printItems(const vector<int>& items, const vector<double>& costs) {
+	for (size_t i = 0; i < items.size(); i++)
 	{
-		double ub = c + (maxWeight - w) * ((i + 1 < items.size()) ?
-			(items[i + 1].cost / items[i + 1].weight) : 0);
-		return ub;
+		cout
+			<< "Item \"" << i + 1
+			<< "\"" << "\n"
+			<< "\tWeight: " << items[i] << "\n"
+			<< "\tCost: " << costs[i]
+			<< "\n";
 	}
-};
+	cout << "\n";
+}
 
-int main()
+void printResult(int maxWeight, double maxCost, const vector<int>& itemsInBackpack)
 {
-	size_t backpackSize = 21;
 
-	vector<Item> items(5);
-	items[0] = Item(5, 10);
-	items[1] = Item(10, 12);
-	items[2] = Item(15, 22);
-	items[3] = Item(7, 8);
-	items[4] = Item(6, 14);
+	cout
+		<< "Maximum weight: "
+		<< maxWeight
+		<< "\n"
+		<< "Maximum cost: "
+		<< maxCost
+		<< "\n" << "\n";
 
-	AbstractBackpack* b = new BranchAndBoundBackpack(backpackSize);
-	vector<unsigned> result = b->SolveUnlimited(items);
+	for (size_t i = 0; i < itemsInBackpack.size(); i++) {
+		cout
+			<< "Number of \""
+			<< i + 1
+			<< "\""
+			<< " is "
+			<< itemsInBackpack[i]
+			<< "\n";
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	srand(static_cast<unsigned>(time(NULL)));
+
+	int maxWeight = 0;
+
+	vector<int> items;
+	vector<double> costs;
+
+	cout << "Enter the maximum weight capacity of the knapsack: ";
+	cin >> maxWeight;
+	cout << "\n";
+
+	items.push_back(3);
+	items.push_back(5);
+	items.push_back(8);
+	costs.push_back(8);
+	costs.push_back(14);
+	costs.push_back(23);
+
+	printItems(items, costs);
+
+	vector<int> itemsInBackpack;
+	double maxCost = unboundedKnapsack(maxWeight, items, costs, itemsInBackpack);
+
+	printResult(maxWeight, maxCost, itemsInBackpack);
+
+	return EXIT_SUCCESS;
 }
