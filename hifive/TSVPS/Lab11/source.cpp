aa58a04b040fca3a7255d02a22c51e12c1cb1f1a
NPanin <subject6gfrt@gmail.com>
2021-12-06 12:37:19 +0700

    TSVPS Lab 12, 17 in proccess; RGR started

--- hifive/TSVPS/Lab11/source.cpp
+++ hifive/TSVPS/Lab11/source.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
 #include <vector>
-#include <tuple>
-#include <bitset>
+#include <map>
 using namespace std;
+
+size_t regSumSub = 0;
+size_t regMultDiv = 0;
+
+size_t fastSumSub = 0;
+size_t fast1bitMult = 0;
+map<size_t, size_t> fastDigitMult;
+map<size_t, size_t> fastDigitMultOverflow;
+
+size_t fastRecCounter = 0;
+
+const bool IS_MULTIPLY_DEBUG = false;
+
+int ctoi(const unsigned char& num) {
+	return num - '0';
+}
+
+char itoc(const int& num) {
+	return static_cast<unsigned char>(num + '0');
+}
 
 string paddedWithZerosToSizeFront(const string& str, size_t size) {
 	if (str.size() >= size) {
@@ -22,149 +40,230 @@ tuple<const string, const string> splitIntoTwoSegments(const string& str, size_t
 	return make_tuple(str.substr(0, size), str.substr(size));
 }
 
-char itoc(const int& num) {
-    return static_cast<unsigned char>(num + '0');
-}
+string regularMultiply(const string& first, const string& second) {
+	vector<short> calculatedMult(first.size() + second.size(), 0);
 
-int ctoi(const unsigned char& number) {
-	return (number - '0');
-}
+	string first_(first);
+	string second_(second);
 
-string add(string x, string y) {
-    const size_t n = max(x.size(), y.size());
-    unsigned short carry = 0;
-    unsigned short sum_col;
-    string result;
+	reverse(first_.begin(), first_.end());
+	reverse(second_.begin(), second_.end());
 
-    x = paddedWithZerosToSizeFront(x, n);
-    y = paddedWithZerosToSizeFront(y, n);
+	for (size_t i = 0; i < first_.size(); ++i) {
+		short carry = 0;
 
-    for (size_t i = n - 1; i != numeric_limits<size_t>::max(); i--) {
-        sum_col = (x[i] - '0') + (y[i] - '0') + carry;
-        carry = sum_col / 10;
-        result.insert(0, to_string(sum_col % 10));
-    }
-
-    if (carry) {
-        result.insert(0, to_string(carry));
-    }
-
-    return result.erase(0, min(result.find_first_not_of('0'), result.size() - 1));
-}
-
-string subtract(string x, string y) {
-    const size_t n = max(x.size(), y.size());
-    short diff;
-    string result;
-
-    while (x.size() < n)
-        x.insert(0, "0");
-
-    while (y.size() < n)
-        y.insert(0, "0");
-
-    for (long long i = n - 1; i >= 0; i--) {
-        diff = ctoi(x[i]) - ctoi(y[i]);
-
-        if (diff >= 0) {
-            result.insert(0, to_string(diff));
-        }
-        else {
-            for (long long j = i - 1; j >= 0; j--) {
-                x[j] = itoc((ctoi(x[j]) - 1) % 10);
-
-                if (x[j] != '9') {
-                    break;
-                }
-            }
-            result.insert(0, to_string(diff + 10));
-        }
-    }
-
-    return result.erase(0, min(result.find_first_not_of('0'), result.size() - 1));
-}
-
-string regularMultiply(string first, string second) {
-	vector<int> calculatedMult(first.size() + second.size(), 0);
-
-	reverse(first.begin(), first.end());
-	reverse(second.begin(), second.end());
-
-	for (size_t i = 0; i < first.size(); ++i) {
-		int carry = 0;
-
-		for (size_t j = 0; j < second.size(); j++) {
-			calculatedMult[i + j] += carry + ctoi(first[i]) * ctoi(second[j]);
+		for (size_t j = 0; j < second_.size(); j++) {
+			calculatedMult[i + j] += carry + ctoi(first_[i]) * ctoi(second_[j]);
 			carry = calculatedMult[i + j] / 10;
 			calculatedMult[i + j] %= 10;
-		}
 
-		calculatedMult[i + second.size()] += carry;
+			regSumSub += 2;
+			regMultDiv += 3;
+		}
+		calculatedMult[i + second_.size()] += carry;
+		regSumSub += 1;
 	}
 
 	reverse(calculatedMult.begin(), calculatedMult.end());
 
-	string result = "";
-	for (const auto& i : calculatedMult) {
+	string result("");
+	for (const short& i : calculatedMult) {
 		result += to_string(i);
 	}
+	result.erase(0, result[0] == '0');
 
-	return (result[0] == '0') ? result.erase(0, 1) : result;
+	return result;
 }
 
-string fastMultiply(string first, string second) {
-    string x(first);
-    string y(second);
+string add(string x, string y) {
+	const size_t n = max(x.size(), y.size());
+	unsigned short carry = 0;
+	unsigned short sum_col;
+	string result;
 
-    const size_t n = max(x.size(), y.size());
+	x = paddedWithZerosToSizeFront(x, n);
+	y = paddedWithZerosToSizeFront(y, n);
 
-    if (n == 1) {
-        return to_string(ctoi(x[0]) * ctoi(y[0]));
-    }
+	for (size_t i = n - 1; i != numeric_limits<size_t>::max(); i--) {
+		sum_col = (x[i] - '0') + (y[i] - '0') + carry;
+		carry = sum_col / 10;
+		result.insert(0, to_string(sum_col % 10));
+	}
 
-    const size_t k = n >> 1;
-    const string halfNeededZeroes = string(n - k, '0');
+	if (carry) {
+		result.insert(0, to_string(carry));
+	}
 
-    x = paddedWithZerosToSizeFront(x, n);
-    y = paddedWithZerosToSizeFront(y, n);
-
-    auto [a, b] = splitIntoTwoSegments(x, k);
-    auto [c, d] = splitIntoTwoSegments(y, k);
-
-    string u = fastMultiply(add(a, b), add(c, d));
-    string v = fastMultiply(a, c);
-    string w = fastMultiply(b, d);
-
-    string uvwSub = subtract(u, add(v, w));
-
-    v = paddedWithZerosByNumberBack(v, 2 * (n - k));
-    uvwSub = paddedWithZerosByNumberBack(uvwSub, (n - k));
-
-    string result = add(add(v, w), uvwSub);
-
-    return result.erase(0, min(result.find_first_not_of('0'), result.size() - 1));
+	return result.erase(0, min(result.find_first_not_of('0'), result.size() - 1));
 }
 
-void callAndLogMultiply(string a, string b, string (*function)(string, string)) {
-	cout << a << ", " << b << ": " << function(a, b) << endl;
+string subtract(string x, string y) {
+	const size_t n = max(x.size(), y.size());
+	short diff;
+	string result;
+
+	x = paddedWithZerosToSizeFront(x, n);
+	y = paddedWithZerosToSizeFront(y, n);
+
+	for (long long i = n - 1; i >= 0; i--) {
+		diff = ctoi(x[i]) - ctoi(y[i]);
+
+		if (diff >= 0) {
+			result.insert(0, to_string(diff));
+		}
+		else {
+			for (long long j = i - 1; j >= 0; j--) {
+				x[j] = itoc((ctoi(x[j]) - 1) % 10);
+
+				if (x[j] != '9') {
+					break;
+				}
+			}
+			result.insert(0, to_string(diff + 10));
+		}
+	}
+
+	return result.erase(0, min(result.find_first_not_of('0'), result.size() - 1));
+}
+
+size_t checkDigit(string a, string b) {
+	return max(a.size(), b.size());
+}
+
+int checkOverflowCount(string a, string b, string c, string d) {
+	int digitsOverflow = 0;
+
+	if (add(a, b).size() > max(a.size(), b.size())) {
+		digitsOverflow++;
+	}
+
+	if (add(c, d).size() > max(a.size(), b.size())) {
+		digitsOverflow++;
+	}
+
+	return digitsOverflow;
+}
+
+string checkOverflow(string a, string b, string c, string d) {
+	int digitsOverflow = checkOverflowCount(a, b, c, d);
+
+	if (digitsOverflow == 1) {
+		return "with 1 overflow";
+	}
+	else if (digitsOverflow == 2) {
+		return "with 2 overflows";
+	}
+	else {
+		return "without overflow";
+	}
+}
+
+string fastMultiplyAction(const string& first, const string& second, int overflow = 0) {
+	string x(first);
+	string y(second);
+
+	const size_t n = max(x.size(), y.size());
+	const size_t k = n >> 1;
+
+	if (n == 1) {
+		fast1bitMult += 1;
+		return to_string(ctoi(x[0]) * ctoi(y[0]));
+	}
+
+	const string halfNeededZeroes = string(n - k, '0');
+
+	x = paddedWithZerosToSizeFront(x, n);
+	y = paddedWithZerosToSizeFront(y, n);
+
+	auto [a, b] = splitIntoTwoSegments(x, k);
+	auto [c, d] = splitIntoTwoSegments(y, k);
+
+	string res;
+	if (overflow >= 2) {
+		string a1b1 = static_cast<bool>(stoll(a) * stoll(c)) ? paddedWithZerosByNumberBack("", 2 * (n - k)) : "0";
+		string a2 = static_cast<bool>(stoll(c)) ? b : "0";
+		string b2 = static_cast<bool>(stoll(a)) ? d : "0";
+		string brackets = add(a2, b2);
+		brackets = paddedWithZerosByNumberBack(brackets, (n - k));
+		string a2b2 = fastMultiplyAction(b, d);
+		fastDigitMultOverflow[checkDigit(x, y)]++;
+		res = add(add(a1b1, brackets), a2b2);
+		return res;
+	}
+
+	string u = fastMultiplyAction(add(a, b), add(c, d), checkOverflowCount(a, b, c, d));
+	string v = fastMultiplyAction(a, c);
+	string w = fastMultiplyAction(b, d);
+
+	string uvwSub = subtract(u, add(v, w));
+
+	fastSumSub += 4;
+
+	if (IS_MULTIPLY_DEBUG) {
+		cout
+			<< "\n"
+			<< "U" << fastRecCounter << ": " << "(" << a << " + " << b << ")" << "(" << c << " + " << d << ")" << " = " << add(a, b) << " * " << add(c, d)
+			<< " (" << checkDigit(a, b) << "-bit " << checkOverflow(a, b, c, d) << ")" << "\n"
+			<< "V" << fastRecCounter << ": " << a << " * " << c << " = " << v
+			<< " (" << checkDigit(a, c) << "-bit " << checkOverflow(a, "0", c, "0") << ")" << "\n"
+			<< "W" << fastRecCounter << ": " << b << " * " << d << " = " << w
+			<< " (" << checkDigit(b, d) << "-bit " << checkOverflow(b, "0", d, "0") << ")" << "\n";
+	}
+
+	fastDigitMult[checkDigit(a, b)]++;
+	fastDigitMult[checkDigit(a, c)]++;
+	fastDigitMult[checkDigit(b, d)]++;
+
+	fastRecCounter++;
+
+	v = paddedWithZerosByNumberBack(v, 2 * (n - k));
+	uvwSub = paddedWithZerosByNumberBack(uvwSub, (n - k));
+
+	string result = add(add(v, w), uvwSub);
+
+	if (IS_MULTIPLY_DEBUG) {
+		cout
+			<< "Re" << ": " << result << "\n";
+	}
+
+	return result.erase(0, min(result.find_first_not_of('0'), result.size() - 1));
+}
+
+string fastMultiply(const string& first, const string& second) {
+	return fastMultiplyAction(first, second);
+}
+
+void multiplyWithPrint(string a, string b, string(*function)(const string&, const string&)) {
+	cout << a << " * " << b << " = " << function(a, b) << endl;
+}
+
+void callAndLogMultiply(string a, string b, string(*function)(const string&, const string&)) {
+	cout << a << " * " << b << " = " << function(a, b) << endl;
 }
 
 int main() {
 	cout << "Regular multiply: " << endl;
-	callAndLogMultiply("12345", "2", regularMultiply);
-	callAndLogMultiply("3", "3", regularMultiply);
-	callAndLogMultiply("6463", "21315", regularMultiply);
-	callAndLogMultiply("18", "23", regularMultiply);
-	callAndLogMultiply("625", "625", regularMultiply);
-	callAndLogMultiply("455689967848757865764", "87578121242346457455689967", regularMultiply);
+	callAndLogMultiply("999999", "999999", regularMultiply);
 
 	cout << endl << "Fast multiply: " << endl;
-	callAndLogMultiply("12345", "2", fastMultiply);
-	callAndLogMultiply("3", "3", fastMultiply);
-	callAndLogMultiply("6463", "21315", fastMultiply);
-	callAndLogMultiply("18", "23", fastMultiply);
-	callAndLogMultiply("625", "625", fastMultiply);
-	callAndLogMultiply("455689967848757865764", "87578121242346457455689967", fastMultiply);
+	callAndLogMultiply("999999", "999999", fastMultiply);
+
+	//cout << "Fast multiply" << endl;
+	//cout << "\t1-bit mult: " << fast1bitMult << endl;
+	//for (const auto& el : fastDigitMult) {
+	//	if (el.first != 1) {
+	//		cout << "\t" << el.first << "-bit mult: " << el.second;
+	//		try {
+	//			string out = " of them with overflow: " + to_string(fastDigitMultOverflow.at(el.first));
+	//			cout << out;
+	//		}
+	//		catch (exception)
+	//		{
+
+	//		};
+	//		cout << "\n";
+	//	}
+	//}
 
 	return 0;
 }
--- hifive/TSVPS/RGR/source.cpp
+++ hifive/TSVPS/RGR/source.cpp
@@ -1,229 +1,153 @@
-///////////////////////////////////////////////////////////////////////////////
-//5.
-///////////////////////////////////////////////////////////////////////////////
-//Написать программу для решения задачи коммивояжёра с помощью метода ветвей и границ.
-//Интерфейс должен позволять вводить количество городов (вершин графа) и значения
-//элементов матрицы расстояний между городами (матрицы смежности).
-///////////////////////////////////////////////////////////////////////////////
 #include <algorithm>
 #include <iostream>
 #include <limits>
 #include <set>
 #include <vector>
-///////////////////////////////////////////////////////////////////////////////
-typedef size_t                          T_town;
-typedef std::vector     < T_town    >   T_path_val;
-typedef int                             T_dist;
-typedef std::vector     < T_dist    >   T_row;
-typedef std::vector     < T_row     >   T_matr;
-typedef std::set        < T_town    >   T_towns;
-///////////////////////////////////////////////////////////////////////////////
-class   T_path
+using namespace std;
+
+typedef size_t T_town;
+typedef vector<T_town> T_path_val;
+typedef vector<int> T_row;
+typedef vector<T_row> T_matr;
+typedef set<T_town> T_towns;
+
+class T_path
 {
-    //-------------------------------------------------------------------------
-    T_matr      dist_matr_;
+    T_matr dist_matr_;
 
-    T_path_val  path_cur_;
-    T_dist      path_cur_len_;
+    T_path_val currentPath;
+    int currentPathLen;
 
-    T_path_val  shortest_path_;
-    T_dist      shortest_path_len_;
+    T_path_val shortestPath;
+    int shortestPathLen;
 
-    T_towns     free_towns_;
-    //-------------------------------------------------------------------------
+    T_towns freeTowns;
+
 public:
-    //-------------------------------------------------------------------------
-    T_path(T_matr  const& dist_matr)
-        :
-        dist_matr_(dist_matr),
-
-        shortest_path_len_(
-            std::numeric_limits< T_dist >::max()
-        )
-    {
-        for (T_town t{}; t < dist_matr.size(); ++t)
-        {
-            free_towns_.insert(t);
+    T_path(T_matr const &dist_matr) : dist_matr_(dist_matr), shortestPathLen(numeric_limits<int>::max()) {
+        for (T_town t{}; t < dist_matr.size(); ++t) {
+            freeTowns.insert(t);
         }
 
         push_town_with_delta_dist(0);
     }
-    //-------------------------------------------------------------------------
-    void    find_and_print_shortest()
-    {
-        find_shortest();
-        print_shortest();
-    }
-    //-------------------------------------------------------------------------
-private:
-    //-------------------------------------------------------------------------
-    void    push_town_with_delta_dist
-    (
-        T_town  town,
-        T_dist  delta_dist = 0
-    )
-    {
-        path_cur_.emplace_back(town);
-        free_towns_.erase(town);
-        path_cur_len_ += delta_dist;
-    }
-    //-------------------------------------------------------------------------
-    void    find_shortest()
-    {
-        do
-        {
-            if (
-                successfully_fill_better_path()
-                )
-            {
-                shortest_path_ = path_cur_;
-                shortest_path_len_ = path_cur_len_;
-            }//if
-        } while (
-            successfully_inc_back_town()
-            );
-    }
-    //-------------------------------------------------------------------------
-    bool    successfully_fill_better_path()
-    {
-        while (
-            !path_is_full()
-            && successfully_push_min_good_town()
-            );
 
-        return  path_is_full();
+    void find_shortest() {
+        do {
+            if (successfully_fill_better_path()) {
+                shortestPath = currentPath;
+                shortestPathLen = currentPathLen;
+            }
+        } while (successfully_inc_back_town());
     }
-    //-------------------------------------------------------------------------
-    bool    path_is_full()
-    {
-        return      path_cur_.size()
-            >= dist_matr_.size() + 1;
-    }
-    //-------------------------------------------------------------------------
-    bool    successfully_push_min_good_town()
-    {
-        return      path_cur_.size()
-            == dist_matr_.size()
-            ? successfully_push_good_town(0)
-            : successfully_push_good_min_free_town_not_less_than(0);
-    }
-    //-------------------------------------------------------------------------
-    bool    successfully_push_good_town(T_town     town)
-    {
-        auto    delta_dist = dist_matr_[path_cur_.back()]
-            [town];
 
+    void print_shortest() {
+        cout << endl;
 
-        bool    bool_res = path_cur_len_
-            + delta_dist
-
-            < shortest_path_len_;
-
-        if (bool_res)
-        {
-            push_town_with_delta_dist
-            (
-                town,
-                delta_dist
-            );
+        for (auto town : shortestPath) {
+            cout << town + 1 << '\t';
         }
 
-        return  bool_res;
+        cout << endl;
     }
-    //-------------------------------------------------------------------------
-    bool successfully_push_good_min_free_town_not_less_than(T_town  town_start) {
-        for (auto i = free_towns_.lower_bound(town_start); i != free_towns_.end(); i++) {
+
+private:
+    void push_town_with_delta_dist(T_town town, int delta_dist = 0) {
+        currentPath.emplace_back(town);
+        freeTowns.erase(town);
+        currentPathLen += delta_dist;
+    }
+
+    bool successfully_fill_better_path() {
+        while (!path_is_full() && successfully_push_min_good_town());
+
+        return path_is_full();
+    }
+
+    bool path_is_full() {
+        return currentPath.size() >= dist_matr_.size() + 1;
+    }
+
+    bool successfully_push_min_good_town() {
+        return currentPath.size() == dist_matr_.size()
+                   ? successfully_push_good_town(0)
+                   : successfully_push_good_min_free_town_not_less_than(0);
+    }
+
+    bool successfully_push_good_town(T_town town) {
+        auto delta_dist = dist_matr_[currentPath.back()][town];
+
+        bool bool_res = currentPathLen + delta_dist < shortestPathLen;
+
+        if (bool_res) {
+            push_town_with_delta_dist(town, delta_dist);
+        }
+
+        return bool_res;
+    }
+
+    bool successfully_push_good_min_free_town_not_less_than(T_town town_start) {
+        for (auto i = freeTowns.lower_bound(town_start); i != freeTowns.end(); i++) {
             if (successfully_push_good_town(*i)) return true;
         }
 
         return false;
     }
-    //-------------------------------------------------------------------------
-    bool    successfully_inc_back_town()
-    {
-        return          path_cur_.size()
-            > 1
 
-            && (
-                successfully_push_good_min_free_town_not_less_than
-                (
-                    pop_and_get_town() + 1
-                )
-
-                || successfully_inc_back_town()
-                );
+    bool successfully_inc_back_town() {
+        return currentPath.size() > 1
+            && (successfully_push_good_min_free_town_not_less_than(pop_and_get_town() + 1) || successfully_inc_back_town());
     }
-    //-------------------------------------------------------------------------
-    T_town  pop_and_get_town()
-    {
-        auto    back_town = path_cur_.back();
-        path_cur_.pop_back();
 
-        if (back_town)
-        {
-            free_towns_.insert(back_town);
+    //// what the fuck is this style
+    //bool successfully_inc_back_town() {
+    //    return currentPath.size() > 1
+
+    //        && (successfully_push_good_min_free_town_not_less_than(
+    //            pop_and_get_town() + 1)
+
+    //            || successfully_inc_back_town());
+    //}
+
+    T_town pop_and_get_town() {
+        auto back_town = currentPath.back();
+        currentPath.pop_back();
+
+        if (back_town) {
+            freeTowns.insert(back_town);
         }
 
-        auto    penultimate_town = path_cur_.back();
+        auto penultimate_town = currentPath.back();
 
-        path_cur_len_ -= dist_matr_[penultimate_town]
-            [back_town];
+        currentPathLen -= dist_matr_[penultimate_town][back_town];
 
-        return  back_town;
+        return back_town;
     }
-    //-------------------------------------------------------------------------
-    void    print_shortest()
-    {
-        std::cout << std::endl;
-
-        for (auto town : shortest_path_)
-        {
-            std::cout << town + 1
-                << '\t';
-        }//for
-
-        std::cout << std::endl;
-    }
-    //-------------------------------------------------------------------------
 };
-///////////////////////////////////////////////////////////////////////////////
-int     main()
-{
-    std::ios::sync_with_stdio(false);
-    int     towns_total{};
 
-    do
-    {
-        std::cout << "Towns total (>= 2): ";
-        std::cin >> towns_total;
-    } while (towns_total < 2);
+int main() {
+    ios::sync_with_stdio(false);
 
-    std::cout << "Enter distances between towns:"
-        << std::endl;
+    int towns_total = 0;
+    cout << "Enter towns (2 or more): ";
+    cin >> towns_total;
+    if (towns_total < 2) { return EXIT_FAILURE; }
 
-    T_matr          dist_matr(
-        towns_total,
-        T_row(towns_total)
-    );
+    cout << "Enter distances between towns:" << endl;
 
-    for (T_town L{}; L < T_town(towns_total); ++L)
-    {
-        for (T_town R{}; R < T_town(towns_total); ++R)
-        {
-            if (L == R)
-            {
-                continue;
-            }
+    T_matr dist_matr(towns_total, T_row(towns_total));
+    for (T_town L{}; L < T_town(towns_total); ++L) {
+        for (T_town R{}; R < T_town(towns_total); ++R) {
+            if (L == R) { continue; }
+            cout << L + 1 << " to " << R + 1 << ":\t";
+            cin >> dist_matr[L][R];
+        }
+    }
 
-            std::cout << L + 1
-                << " - "
-                << R + 1
-                << "\t: ";
+    T_path path(dist_matr);
 
-            std::cin >> dist_matr[L][R];
-        }//for
-    }//for
+    path.find_shortest();
+    path.print_shortest();
 
-    T_path  path(dist_matr);
-    path.find_and_print_shortest();
+    return EXIT_SUCCESS;
 }
