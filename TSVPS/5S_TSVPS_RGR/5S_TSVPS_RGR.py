6c1ba21c0c70c90fb649ef19d2e36ba7f4af6f56
MangriMen <mangrimen@gmail.com>
2021-12-16 19:05:42 +0700

    TSVPS removed global variables

--- TSVPS/5S_TSVPS_RGR/5S_TSVPS_RGR.py
+++ TSVPS/5S_TSVPS_RGR/5S_TSVPS_RGR.py
@@ -1,9 +1,3 @@
-regSum = 0
-regMul = 0
-fastSum = 0
-fastMul = 0
-
-
 def getMQuarter(A, num):
     if isinstance(A, list):
         matrixSize = len(A)
@@ -44,82 +38,83 @@ def getMQuarter(A, num):
     return A
 
 
-def minus(A, B):
-    global fastSum, fastMul
-
+def minus(A, B, counters):
     if isinstance(A, list) and isinstance(B, list):
         Q = [[0 for _ in range(len(A))] for _ in range(len(A))]
 
         for i in range(len(Q)):
             for j in range(len(Q)):
                 Q[i][j] = A[i][j] - B[i][j]
-                fastSum += 1
+                counters['sum'] += 1
         return Q
 
-    fastSum += 1
+    counters['sum'] += 1
     return A - B
 
 
-def plus(A, B):
-    global fastSum, fastMul
-
+def plus(A, B, counters):
     if isinstance(A, list) and isinstance(B, list):
         Q = [[0 for _ in range(len(A))] for _ in range(len(A))]
 
         for i in range(len(Q)):
             for j in range(len(Q)):
                 Q[i][j] = A[i][j] + B[i][j]
-                fastSum += 1
+                counters['sum'] += 1
         return Q
 
-    fastSum += 1
+    counters['sum'] += 1
     return A + B
 
 
-def mul(A, B):
-    global fastMul
-
+def mul(A, B, counters):
     if isinstance(A, list) and isinstance(B, list):
-        return fastMatrixMultiply(A, B)
+        return fastMatrixMultiply(A, B, counters)
 
-    fastMul += 1
+    counters['mul'] += 1
     return A * B
 
 
-def fastMatrixMultiply(A, B):
+def fastMatrixMultiply(A, B, counters):
     M1 = mul(
-        minus(getMQuarter(A, 2), getMQuarter(A, 4)),
-        plus(getMQuarter(B, 3), getMQuarter(B, 4))
+        minus(getMQuarter(A, 2), getMQuarter(A, 4), counters),
+        plus(getMQuarter(B, 3), getMQuarter(B, 4), counters),
+        counters
     )
     M2 = mul(
-        plus(getMQuarter(A, 1), getMQuarter(A, 4)),
-        plus(getMQuarter(B, 1), getMQuarter(B, 4))
+        plus(getMQuarter(A, 1), getMQuarter(A, 4), counters),
+        plus(getMQuarter(B, 1), getMQuarter(B, 4), counters),
+        counters
     )
     M3 = mul(
-        minus(getMQuarter(A, 1), getMQuarter(A, 3)),
-        plus(getMQuarter(B, 1), getMQuarter(B, 2))
+        minus(getMQuarter(A, 1), getMQuarter(A, 3), counters),
+        plus(getMQuarter(B, 1), getMQuarter(B, 2), counters),
+        counters
     )
     M4 = mul(
-        plus(getMQuarter(A, 1), getMQuarter(A, 2)),
-        getMQuarter(B, 4)
+        plus(getMQuarter(A, 1), getMQuarter(A, 2), counters),
+        getMQuarter(B, 4),
+        counters
     )
     M5 = mul(
         getMQuarter(A, 1),
-        minus(getMQuarter(B, 2), getMQuarter(B, 4)),
+        minus(getMQuarter(B, 2), getMQuarter(B, 4), counters),
+        counters
     )
     M6 = mul(
         getMQuarter(A, 4),
-        minus(getMQuarter(B, 3), getMQuarter(B, 1)),
+        minus(getMQuarter(B, 3), getMQuarter(B, 1), counters),
+        counters
     )
     M7 = mul(
-        plus(getMQuarter(A, 3), getMQuarter(A, 4)),
-        getMQuarter(B, 1)
+        plus(getMQuarter(A, 3), getMQuarter(A, 4), counters),
+        getMQuarter(B, 1),
+        counters
     )
 
-    C1 = plus(minus(plus(M1, M2), M4), M6)
-    C2 = plus(M4, M5)
-    C3 = plus(M6, M7)
-    C4 = minus(plus(minus(M2, M3), M5), M7)
+    C1 = plus(minus(plus(M1, M2, counters), M4, counters), M6, counters)
+    C2 = plus(M4, M5, counters)
+    C3 = plus(M6, M7, counters)
+    C4 = minus(plus(minus(M2, M3, counters), M5, counters), M7, counters)
 
     if isinstance(C1, list):
         C = [[] for _ in range(len(A))]
@@ -135,48 +130,51 @@ def fastMatrixMultiply(A, B):
     else:
         C = [[C1, C2], [C3, C4]]
 
+    counters['sum'] += 18
+    counters['mul'] += 7
+
     return C
 
 
-def dgemmBlas(A, B):
-    global regSum, regMul
+def dgemmBlas(A, B, counters):
     C = [[0 for _ in range(len(A))] for _ in range(len(A))]
 
     for i in range(len(A)):
         for j in range(len(A)):
             for k in range(len(A)):
                 C[i][j] += A[i][k] * B[k][j]
-                regSum += 1
-                regMul += 1
+                counters['sum'] += 1
+                counters['mul'] += 1
     return C
 
 
 def main():
-    global regSum, regMul, fastSum, fastMul
-
     matrixSize = 2
     with open('out.csv', 'w+') as file:
         file.write("Size;Regular +;Regular *;Fast +;Fast *;Regular total;Fast total\n")
         while matrixSize <= 512:
             print("Processing: ", matrixSize, "*", matrixSize, "...", sep=" ")
-            regSum = 0
-            regMul = 0
-            fastSum = 0
-            fastMul = 0
-
             A = [[pow(-1, x + y) for x in range(matrixSize)] for y in range(matrixSize)]
             B = [[x + y for x in range(matrixSize)] for y in range(matrixSize)]
 
-            dgemmBlas(A, B)
-            fastMatrixMultiply(A, B)
+            regCounters = dict()
+            regCounters['sum'] = 0
+            regCounters['mul'] = 0
+
+            fastCounters = dict()
+            fastCounters['sum'] = 0
+            fastCounters['mul'] = 0
+
+            dgemmBlas(A, B, regCounters)
+            fastMatrixMultiply(A, B, fastCounters)
 
             outStr = str(matrixSize) + " x " + str(matrixSize) + ";" \
-                + str(regSum) + ";" \
-                + str(regMul) + ";" \
-                + str(fastSum) + ";" \
-                + str(fastMul) + ";" \
-                + str(regSum + regMul) + ";" \
-                + str(fastSum + fastMul) + ";" \
+                + str(regCounters['sum']) + ";" \
+                + str(regCounters['mul']) + ";" \
+                + str(fastCounters['sum']) + ";" \
+                + str(fastCounters['mul']) + ";" \
+                + str(regCounters['sum'] + regCounters['mul']) + ";" \
+                + str(fastCounters['sum'] + fastCounters['mul']) + ";" \
                 + "\n"
 
             file.write(outStr)
